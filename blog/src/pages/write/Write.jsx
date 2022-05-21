f1eb93134dd18acada2ae3ea3d8d88701c82a2d0
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2022-05-21 11:39:28 +0200

    write Page

--- blog/src/pages/write/Write.jsx
+++ blog/src/pages/write/Write.jsx
@@ -14,7 +14,7 @@ export default function Write() {
                     <input type="text" placeholder="Title" className="writeInput" autoFocus="true"/>
                 </div>
                 <div className="writeFormGroup">
-                    <textarea type="text" placeholder="Tell yout story . . ."  className="writeInput writeText"/>
+                    <textarea type="text" placeholder="Tell your story . . ."  className="writeInput writeText"/>
                 </div>
                 <button className="writeSubmit">Publish</button>
             </form>
