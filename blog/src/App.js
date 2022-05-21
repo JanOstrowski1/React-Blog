b461f2fb89c45d188b2c41ef78e764840226cdcd
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2022-05-21 13:11:45 +0200

    finished layouts

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,6 +3,9 @@ import Home from './pages/home/Home';
 import Single from './pages/single/Single';
 import TopBar from "./components/topbar/TopBar";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -11,8 +14,12 @@ function App() {
       <TopBar/>
       {/*<Home/>*/}
       {/*<Single/>*/}
-          <Write/>
+      {/*    <Write/>*/}
+      {/*<Settings/>*/}
+      {/*    <Login/>*/}
+          <Register/>
       </>
+
     </div>
   );
 }
