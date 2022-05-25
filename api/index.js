64e73506bbb76c851511ab56051a563b8d70504f
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2022-05-25 20:35:40 +0200

    auth router config

--- api/index.js
+++ api/index.js
@@ -1,18 +1,23 @@
-const express=require("express");
-const app= express();
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URL)
     .then(console.log("connected to Mongo DB"))
-    .catch((err)=>console.log(err));
+    .catch((err) => console.log(err));
 
 
+    app.use("/api/auth" , authRoute);
 
-app.listen("5000", ()=>{
-    console.log("Backend is running.");
+
+app.listen("5000", () => {
+        console.log("Backend is running.");
     }
-)
+);
+
