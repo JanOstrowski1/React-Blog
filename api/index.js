03a29d55031929d221f3cabff61dd2ec4f59cbce
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2022-05-26 21:05:12 +0200

    some issues with update

--- api/index.js
+++ api/index.js
@@ -3,17 +3,19 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 dotenv.config();
 app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URL)
-    .then(console.log("connected to Mongo DB"))
+    .then(()=>console.log("connected to Mongo DB"))
     .catch((err) => console.log(err));
 
 
     app.use("/api/auth" , authRoute);
+    app.use("/api/users" , userRoute);
 
 
 app.listen("5000", () => {
