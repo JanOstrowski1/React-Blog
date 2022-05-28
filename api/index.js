16bd4c0f97192298ce125430bb156c6b9c0550f7
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2022-05-28 11:34:36 +0200

    finnished REST API

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
 const categoriesRoute = require("./routes/categories");
+const multer= require("multer");
+
 
 dotenv.config();
 app.use(express.json());
@@ -16,10 +18,23 @@ mongoose
     .catch((err) => console.log(err));
 
 
-    app.use("/api/auth" , authRoute);
-    app.use("/api/users" , userRoute);
-    app.use("/api/posts" , postsRoute);
-    app.use("/api/categories" , categoriesRoute);
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images")
+    },filename:(req,file,cb)=>{
+        cb(null,"file.jpeg");
+    },
+});
+
+const upload = multer({storage:storage});
+app.post("api/upload", upload.single("file"), (req,res)=>{
+    res.status(200).json("File has been uploaded");
+})
+
+app.use("/api/auth" , authRoute);
+app.use("/api/users" , userRoute);
+app.use("/api/posts" , postsRoute);
+app.use("/api/categories" , categoriesRoute);
 
 
 app.listen("5000", () => {
